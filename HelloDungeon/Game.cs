08a38c4a60467a018655f3829f5cc2646e95fde1
Christian Deese <s238007@students.aie.edu>
2023-08-23 15:07:22 -0500

    added welcome message and name input

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -11,6 +11,29 @@ namespace HelloDungeon
         public void Run()
         {
 
+            //create and initialize variables
+            string playerName;
+            int playerHealth = 100;
+            bool isAlive = true;
+            float storyProgress = 3.3f;
+            int stage;
+
+            //get first and last name
+
+            Console.WriteLine("Enter First Name:");
+            string firstName = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Enter Last Name:");
+            string lastName = Console.ReadLine();
+
+            Console.Clear();
+
+            //combine and print
+            playerName = firstName + " " + lastName;
+            Console.WriteLine("Welcome " + playerName);
         }
+
     }
 }
