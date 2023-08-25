4a6756111fd7acf6659ab78203530dd218a32916
Christian Deese <s238007@students.aie.edu>
2023-08-25 12:00:18 -0500

    added unfinished scenarios and option to continue

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -18,6 +18,9 @@ namespace HelloDungeon
             float storyProgress = 3.3f;
             int stage;
 
+            //player stats
+            int strength = 0;
+
             //get first and last name
 
             Console.WriteLine("Enter Name:");
@@ -41,6 +44,38 @@ namespace HelloDungeon
             Console.WriteLine("B. Wake up");
             Console.WriteLine("C. Go back to sleep");
 
+            //choice mechanics
+            string playerChoice = Console.ReadLine();
+            bool combatInitiated = false;
+            if(playerChoice == "A")
+            {
+                //fight
+                Console.WriteLine("You throw a jab straight ahead with full force into the figure's center.");
+                Console.WriteLine("The figure stumbles back and you're able to hop on your feet.");
+                Console.WriteLine("STRENGTH UP!!");
+                strength++;
+                combatInitiated = true;
+            }
+            else 
+            {
+                if(playerChoice == "B")
+                {
+                    //dodge
+                    Console.WriteLine("You open your eyes and there's a figure rearing for a punch.");
+                    Console.WriteLine("You move your head out of the way just in time and you roll out from under him.");
+                    combatInitiated = true;
+                }
+                else if(playerChoice == "C")
+                 {
+                    //next stage B
+                    Console.WriteLine("There's a whoosh of air and you lose consciousness again.");
+                 }
+
+                //Option to Continue
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey(true);
+                Console.Clear();
+            }
             
         }
 
